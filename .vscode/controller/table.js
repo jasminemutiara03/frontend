7c6a7dd61cfe138bd6143462e5be2254f42a3a7d
jasminemutiara03 <bjasminemutiara@gmail.com>
2023-03-19 18:12:32 +0700

    add boiller

--- .vscode/controller/table.js
+++ .vscode/controller/table.js
@@ -1,14 +1,8 @@
-import { get } from "https://jscroot.github.io/api/croot.js";
-import {setInner,addChild } from "https://jscroot.github.io/element/croot.js";
+import { addChild } from "https://jscroot.github.io/element/croot.js";
 import {getRandomColor,getRandomColorName} from "https://jscroot.github.io/image/croot.js";
-import {presensiTag,presensiClass,presensiContent} from "./template/table.js"
+import {presensiTag,presensiClass,presensiContent} from "../template/table.js";
 
-
-let URLPresensi = "https://gocroot.herokuapp.com/presensi";
-
-get(URLPresensi,isiTablePresensi);
-
-function isiTablePresensi(results){
+export function isiTablePresensi(results){
     results.forEach(isiRow);
 }
 
@@ -16,6 +10,3 @@ function isiRow(value){
     let content=presensiContent.replace("#NAMA#",value.Biodata.Nama).replace("#PHONENUMBER#",value.Phone_number).replace("#LOKASI#",value.Location).replace("#KET#",value.Checkin).replace("#MASUK#",value.Datetime).replace("#PULANG#",value.Datetime).replace("#DURASI#",value.Datetime).replace("#WARNA#",getRandomColor()).replace(/#WARNALOGO#/g,getRandomColorName());
     addChild("presensi",presensiTag,presensiClass,content);
 }
-
-
-setInner("namadivisi","Dari croot.js");
